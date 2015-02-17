78126e8776b458287b3d2cfda0e4ad9a11135f7c
nathanlad830 <nathanprentice830@gmail.com>
2015-02-17 20:35:26 +0000

    Create JavaScriptName.js

---
+++ JavaScriptName.js
@@ -0,0 +1,9 @@
+var name = "Nathan";
+var nameLength = name.length;
+
+if (nameLength >= 3){
+document.write("Cool name, incase you forgot your name is, "+name);
+} 
+else {
+document.write("Thats a cool short name, "+name);
+}
